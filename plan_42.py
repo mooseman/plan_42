9e1e9e11062c2158ac0ef8362b2687374cfe2722
Andy Elvey <andy@obsidian.(none)>
2009-05-22 20:26:34 +1200

    First commit.

---
+++ plan_42.py
@@ -0,0 +1,31 @@
+
+#  Have you been looking for a public domain operating system? 
+#  The perfect public domain OS? One which treats everything as a file? 
+
+#  Well, your search will have to go on for a bit longer. In the 
+#  meantime though, you can play with Plan 42.....  ;)    
+
+#  This code is released to the publ;ic domain.  
+
+#  This code requires Teagit.  
+
+#  Notes to self - commands to star with - plumb, cat, 
+#  namespace, ......
+#  Also do kernel services.  
+
+#  Teagit will be the toy "file system".  
+
+
+from teagit import * 
+
+
+
+
+
+
+
+
+
+
+
+
