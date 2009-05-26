87d54499e65a3f27a9d02a9c537b1c5fab1a7e5a
Andy Elvey <andy@obsidian.(none)>
2009-05-26 19:38:16 +1200

    More code.

--- plan_42.py
+++ plan_42.py
@@ -17,24 +17,30 @@ class logon():
       self.username = username 
       self.pw = pw 
 	 
-      os = os.lower()
+      os = os.lower().translate(None, ' ')  
+	  
+      if os == "plan42": 
+	     # Do some stuff to imitate setup during boot 
+         print "Welcome to Plan 42! Share and enjoy.... " 
+         time.sleep(3) 
+         print "Mounting drives... please wait..." 
+         time.sleep(3) 
+         print "Mounted /  /dev  /usr  /etc  "  
+         time.sleep(3) 
+         print "Done!"  
 	 
-	  # Do some stuff to imitate setup during boot 
-      print "Welcome to Plan 42! Share and enjoy.... " 
-      time.sleep(3) 
-      print "Mounting drives... please wait..." 
-      time.sleep(3) 
-      print "Mounted /  /dev  /usr  /etc  "  
-      time.sleep(3) 
-	 
-      if "win" in os: 
+      elif "win" in os: 
           print "Oh, come on now, be serious - use a REAL OS!" 
-          time.sleep(2) 
-          print "Loaded BSOD()  :)  " 		
+          time.sleep(3) 
+          print "Loaded BSOD()  :)  " 	
+          time.sleep(3) 		  	
 
 		 
 #  Run the class 
 a = logon("win", "fred", "foobar") 
+
+b = logon("Plan 42", "trev", "asdf56") 
+
 
 
 
