95246df52f0777bca40fc11594d626aaee11b5fa
Andy Elvey <andy@obsidian.(none)>
2009-08-02 14:39:07 +1200

    More work on the plumber. It now has two classes, one to read plumbing rules and one to run them.

--- plumber/plumber2.py
+++ plumber/plumber2.py
@@ -11,33 +11,62 @@
 #  Note - to open a file in vi and go to a given 
 #  line, do this - vi +5 myfile.txt - goes to line 5. 
 
+#  Note - this command works! It opens nano as expected, on the 
+#  5th line of the file - 
+#  pipe = subprocess.Popen('nano +5 foo.txt', shell=True, bufsize=2000).stdout 
+#  The same command also works with vi.  
+
 #  TO DO - Add code to look at the various plumbing rules 
 #  and act on them.  
 
 
-import os, fileinput, itertools    
+import os, fileinput, itertools, subprocess     
 
-class plumber(): 
+class readplumbrules(object): 
    def init(self):  
       self.mydict = {} 
 	  
-   def open(self, file):
-      self.file = open(file, 'r').readlines()
+   def open(self, rulesfile):
+      self.file = open(rulesfile, 'r').readlines()
       for k, v in enumerate(self.file): 
 	      self.mydict[k] = v.rstrip('\n') 
 		  
    def display(self): 
-      print(self.mydict) 
+      print self.mydict 
 	  	  
 
 #  Run the class 
-myplumb = plumber() 
+'''a = readplumbrules() 
+a.init() 
+a.open('plumbertest1') 
+a.display() '''
 
-myplumb.init() 
 
-myplumb.open('plumbertest1') 
+#  Now, a class to take the read-in rules and act on them.  
+class runrules(readplumbrules): 
+   # define the apps to run the commands 
+   def apps(self): 
+      self.editor = 'nano' 
+                  
+   def run(self, file): 
+      self.fname = file 
+      if self.fname.endswith(self, '.txt', start=0): 
+         pipe = subprocess.Popen("self.editor" '+5' "self.fname", shell=True, bufsize=2000).stdout 
+      else: 
+         pass  
+         
+#  Test the class 
+a = runrules() 
+print dir(a) 
 
-myplumb.display() 
+a.init() 
 
-   
+a.open('plumbertest1') 
+a.display() 
+
+
+                  
+
+
+
 
