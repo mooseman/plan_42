320f89c207cc11a3aa54974814e29e2d6f09c045
mooseman <andy@obsidian.(none)>
2011-01-07 16:40:26 +1300

    Added BPB code. About to start on the second stage bootloader.

--- plan42_boot.asm
+++ plan42_boot.asm
@@ -1,22 +1,93 @@
-;*********************************************
-;	plan42_boot.asm
-;	Bootloader for Plan 42 
-;       
-;       Acknowledgement -  Very many thanks to the 
-;       people who wrote the Operating Systems 
-;       Development series of tutorials at 
-;       brokenthron.com.    
-;*********************************************
+
+;  "Hello world" boot loader by Daniel Faulkner 
+;   Acknowledgements - Many thanks to Daniel on the osdever forum  
+;   and to Mike (of brokenthorn.com).  
+
+;   Compile this code using this command - 
+;   nasm -f bin plan42_boot.asm -o plan42_boot.bin
+
+[BITS 16]      ; 16 bit code generation
+[ORG 0x7C00]   ; Origin location. This MUST be 0x7C00 for a 
+               ; boot-loader. See OSDev article 3 for details - 
+               ; http://www.brokenthorn.com/Resources/OSDev3.html  
+
+; Jump over the parameter block to the main program  
+start: jmp main 
+
+
+;********************************************************
+; BIOS parameter block. This describes the layout of the 
+; filesystem. 
+;******************************************************** 
+TIMES 0Bh-$+start DB 0
+
+bpbBytesPerSector:  	DW 512
+bpbSectorsPerCluster: 	DB 1
+bpbReservedSectors: 	DW 1
+bpbNumberOfFATs: 	    DB 2
+bpbRootEntries: 	    DW 224
+bpbTotalSectors: 	    DW 2880
+bpbMedia: 	            DB 0xF0
+bpbSectorsPerFAT: 	    DW 9
+bpbSectorsPerTrack: 	DW 18
+bpbHeadsPerCylinder: 	DW 2
+bpbHiddenSectors: 	    DD 0
+bpbTotalSectorsBig:     DD 0
+bsDriveNumber: 	        DB 0
+bsUnused: 	            DB 0
+bsExtBootSignature: 	DB 0x29
+bsSerialNumber:	        DD 0xa0a1a2a3
+bsVolumeLabel: 	        DB "PLAN 42    "
+bsFileSystem: 	        DB "FAT12   " 
+
+
+
+; Main program
+main:      ; Label for the start of the main program
+
+mov ax,0x0000   ; Setup the Data Segment register
+      ; Location of data is DS:Offset
+mov ds,ax   ; This can not be loaded directly it has to be in two steps.
+      ; 'mov ds, 0x0000' will NOT work due to limitations on the CPU
+
+mov si, str1  ; Load the string into position for the procedure.
+call PutStr   ; Call/start the procedure
+mov si, str2  ; Load another string
+call PutStr     ;  Print string   
+
+jmp $      ; Never ending loop
+
+; Procedures
+PutStr:      ; Procedure label/start
+; Set up the registers for the interrupt call
+mov ah,0x0E   ; The function to display a chacter (teletype)
+mov bh,0x00   ; Page number
+mov bl,0x07   ; Normal text attribute
+
+.nextchar   ; Internal label (needed to loop round for the next character)
+lodsb      ; I think of this as LOaD String Block
+      ; (Not sure if thats the real meaning though)
+      ; Loads [SI] into AL and increases SI by one
+; Check for end of string '0'
+or al,al   ; Sets the zero flag if al = 0
+      ; (OR outputs 0's where there is a zero bit in the register)
+jz .return   ; If the zero flag has been set go to the end of the procedure.
+      ; Zero flag gets set when an instruction returns 0 as the answer.
+int 0x10   ; Run the BIOS video interrupt
+jmp .nextchar   ; Loop back round tothe top
+.return      ; Label at the end to jump to when complete
+ret      ; Return to main program
+
+; Data
+
+str1 db 'Open the pod-bay door, HAL...',13,10,0
+str2 db "I'm sorry.... I can't do that, Dave. ",13,10,0   
+
+; End Matter
+times 510-($-$$) db 0   ; Fill the rest with zeros
+dw 0xAA55      ; Boot loader signature 
+
  
-org    0x7c00				; We are loaded by BIOS at 0x7C00
-                                                ; org is the origin location.  
- 
-bits	16					; We are in 16 bit Real Mode
- 
-Start:
-	cli					; Clear all Interrupts
-	hlt					; halt the system
-	
-times 510 - ($-$$) db 0				; We have to be 512 bytes. Clear the rest of the bytes with 0
- 
-dw 0xAA55					; Boot Signature  
+cli				; clear the interrupts  
+hlt             ; halt the system 
+
